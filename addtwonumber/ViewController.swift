384d8cd86413f4c1d18eff50e180f51a4807ff93
Bishowjit-Ray <82141518+Bishowjit-Ray@users.noreply.github.com>
2021-04-08 14:54:24 +0600

    addtwonuminIOS

---
+++ addtwonumber/ViewController.swift
@@ -0,0 +1,32 @@
+//
+//  ViewController.swift
+//  addtwonumber
+//
+//  Created by Bishowjit Ray on 26/2/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var output: UIView!
+    
+    @IBOutlet weak var nam1: UITextField!
+    
+    @IBOutlet var nam2: UIView!
+    
+    
+    @IBAction func add(_ sender: Any) {
+        let a=int(nam1.text)
+        
+    }
+    
+    
+    
+    
+    
+    
+    
+
+}
+
